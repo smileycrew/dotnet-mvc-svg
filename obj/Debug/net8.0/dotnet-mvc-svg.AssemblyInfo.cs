123bdf3a180a2f5e4cc7e4cf12494e91fa16dfa7
Edwin Moz <edwin.moz@icloud.com>
2024-06-29 13:47:02 -0500

    used dashes instead of underscores on the connection string

--- obj/Debug/net8.0/dotnet-mvc-svg.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-mvc-svg.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-mvc-svg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd4fbbbbdba3d8b14d780dc36a81970d5aee6d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8a6a210d467ee6f1739aa3bbb8f634ba01fe6ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-mvc-svg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-mvc-svg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
